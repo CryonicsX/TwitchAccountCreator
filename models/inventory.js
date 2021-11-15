087a43d3becda44f12d9dda4d9aab4901bfb6ad3
CRYONICX <80361723+CryonicsX@users.noreply.github.com>
2021-11-15 23:28:32 +0300

    Delete models directory

--- models/inventory.js
+++
@@ -1,6 +0,0 @@
-const { model, Schema } = require('mongoose'); //Developed By CryonicX
-module.exports = model('inventory', new Schema({
-    Guild: String,
-    User: String,
-    Inventory: Object,
-}))
