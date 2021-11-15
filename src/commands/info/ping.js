2319c2964c83dcf6a86bc239e485e02c339ad199
CRYONICX <80361723+CryonicsX@users.noreply.github.com>
2021-11-15 23:28:42 +0300

    Delete src directory

--- src/commands/info/ping.js
+++
@@ -1,8 +0,0 @@
-const { MessageEmbed } = require('discord.js')
-module.exports = {
-    name: 'ping',
-    category: 'info',
-    run: async(client, message, args) => {
-        const msg = await message.channel.send(`🏓 Pinging...`)
-    }
-}
--- src/events/ready.js
+++
@@ -1,6 +0,0 @@
-const client = require('../../src/index'); //Developed By CryonicX
-client.on('ready', () => {
-    client.user.setStatus("online");
-    client.user.setActivity('Developed by CryonicX!')
-    console.log(`${client.user.tag} Bot Giriş Yaptı!`);
-})
